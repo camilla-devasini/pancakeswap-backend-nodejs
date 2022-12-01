8c7828dee5754270c1508244f0332559b4055c2e
Camilla Devasini <camilladevasini@gmail.com>
2022-12-01 15:11:18 +0100

    create and connect to database users_db

---
+++ server/database/db.js
@@ -0,0 +1,24 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+//configuro il mio database:
+
+//creo una istanza pool e passo un oggetto di configurazione
+// faccio riferimento alle environment variables che si trovano in .env
+const pool = new Pool({
+  database: process.env.DATABASE_NAME,
+  //   password: process.env.PASSWORD, non ho impostato una password
+  user: process.env.DATABASE_USER, //è meglio non assegnare un user? altrimenti come lo condividiamo all'interno del team??
+  port: process.env.DATABASE_PORT,
+  host: process.env.DATABASE_HOST,
+});
+
+async function connectToDatabase() {
+  try {
+    await pool.connect();
+    console.log("Connected to database");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = connectToDatabase;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const authRouter = require("./routers/authRouter");
+const connectToDatabase = require("./database/db");
+
+connectToDatabase();
 
 const app = express();
 
