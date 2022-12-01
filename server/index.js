c51241909f059d254688e4d30c9a97751e98ed0b
Camilla Devasini <camilladevasini@gmail.com>
2022-12-01 14:03:34 +0100

    create a router module

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use("/auth", authRouter);
 //tutte le richieste inviate al route del server "/auth"
 // eseguiranno il middleware authRouter = un express middleware
+// stiamo così usando il il router module all'interno di index.js
 
 app.listen(4000, () => {
   console.log("Server listening on port 4000");
--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,3 +1,16 @@
 const express = require("express");
-const router = express.Router();
 const validateForm = require("../controllers/validateForm");
+
+const router = express.Router();
+// siamo in authRouter.js --> abbiamo creato route MODULE chiamato authRouter.js
+
+//questa è una tipica route function definita tramite il metodo POST
+router.post("/login", (req, res) => {
+  validateForm(req, res); //callback function che viene invocata quando si ha una richiesta http post with url path "/login"
+});
+
+router.post("/signup", (req, res) => {
+  validateForm(req, res);
+});
+
+module.exports = router;
