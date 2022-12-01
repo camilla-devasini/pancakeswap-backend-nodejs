dd809daa31806dd458848ce3b424c462b48f8b4c
Camilla Devasini <camilladevasini@gmail.com>
2022-12-01 13:35:00 +0100

    £server
    server setting

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const authRouter = require("./routers/authRouter");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
+//tutte le richieste inviate al route del server "/auth"
+// eseguiranno il middleware authRouter = un express middleware
+
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
